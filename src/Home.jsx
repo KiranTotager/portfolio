73d6ece443a541ed87b080af803dfb2307d1afeb
KiranTotager <tskiran5114@gmail.com>
2025-03-27 23:56:57 +0530

    immage plaicing

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-
+import React from 'react';
 
 export default function Home() {
   return (
-    <div >
-        <img src="/images/my_image.jpeg" alt="myimage" className=''/>
+    <div className="flex items-center justify-center min-h-screen">
+      <img 
+        src="/images/my_image.jpeg" 
+        alt="myimage" 
+        className="w-40 h-40 rounded-full object-cover border-4 border-gray-700 shadow-lg" 
+      />
     </div>
-  )
+  );
 }
